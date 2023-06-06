354facbc1f921a4e1cb3927bda0afd49a3d160de
sunny9312-lab <daisey9312@gmail.com>
2023-06-06 12:36:43 +1000

    first version

--- js/control-flow/challenge/script.js
+++ js/control-flow/challenge/script.js
@@ -11,12 +11,32 @@ if (x > y) {
     console.log("x is equal to y");
 }
 
-let userInput = "abc";
+// let userInput = "abc";
 
-if (userInput = String) {
-    console.log(("input is ") + userInput);
-} else if (userInput = Number) {
-    console.log(userInput = userInput * userInput );
+// userInput = prompt("please enter an input");
+// console.log(userInput);
+
+// if (userInput === String) {
+//     console.log(("input is ") + userInput);
+// } else if (userInput === Number) {
+//     console.log(userInput = userInput * userInput );
+// } else {
+//     console.log("invalid input");
+// }
+// console.log("");
+
+let userInput = true;
+userInput = prompt("please enter an input");
+
+if (typeof userInput === "number"){
+    console.log(userInput)
+} else if (typeof userInput === "string"){
+    console.log(userInput);
 } else {
     console.log("invalid input");
+}
+
+if (parseInt(userInput)){
+    console.log("this is a  number");
+    console.log(userInput ** 2);
 }
