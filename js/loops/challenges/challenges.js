3fc63cd38ea83806afc178f5a3fc1c6ed6177112
sunny9312-lab <daisey9312@gmail.com>
2023-06-09 13:03:15 +1000

    loop is doing

---
+++ js/loops/challenges/challenges.js
@@ -0,0 +1,20 @@
+// const addSum = 10;
+
+// for (let i = 0; i < addSum; ++i) {
+    
+//     addSum = addSum + i;
+// }
+// console.log(addSum);
+
+const shoppinglist = ["aaa", "bbb", "ccc", "ddd"];
+// const listonly= String;
+
+console.log(shoppinglist);
+
+for (let i = 0; i < shoppinglist.length; i++) {
+    // const listonly = shoppinglist[i];
+    // console.log(listonly + "     ");
+    
+
+}
+
