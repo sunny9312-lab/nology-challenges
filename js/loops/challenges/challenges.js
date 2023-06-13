372ba939e40ca8d18a3b52bdf6a288a3ca34ea6b
sunny9312-lab <daisey9312@gmail.com>
2023-06-13 23:49:52 +1000

    loop challenge done

--- js/loops/challenges/challenges.js
+++ js/loops/challenges/challenges.js
@@ -70,6 +70,60 @@ for (let i = 0; i < wordStr.length; i++) {
 
 console.log(newStr);
 
+//ysh 
+// aaa->aAa
 
+const strInput = "aaa";
+let strOutput = "";
+
+for (let i = 0; i < strInput.length ; i++ ) {
+    if (i % 2) {   //odd position
+        strOutput += strInput[i].toUpperCase();
+        console.log("ysh");
+        console.log(`${i}  i`);
+        console.log(`${strInput[i]}  strInput[i]`);
+        
+    } else {
+        strOutput = strOutput + strInput[i];
+        console.log(strInput[i].charCodeAt(0)); 
+        console.log("here!");         
+    }
+}
+
+console.log(strOutput);
+//
+
+//removing vowels
+const stringName = "martyna";
+let stringNameNoVowels = "";
+const vowels = ["a", "e", "i", "o", "u"];
+
+for (let i = 0; i < stringName.length; i++) {
+    let char = stringName[i];
+    if (!vowels.includes(char)) {
+        stringNameNoVowels += char;
+    }
+}
+console.log(stringNameNoVowels, "- this is for part 1");
+
+
+const strName = "martyna";
+let noVowelsName = "";
+const vowelAEIOU = ["a", "e", "i", "o", "u"];
+for (let i = 0; i < strName.length ; i++){
+    if (!vowelAEIOU.includes(strName[i])) {
+        noVowelsName += strName[i];
+    }
+}
+console.log(noVowelsName, "-eliminate vowls in name");
+
+// Loop through an array backwards
+
+const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = arrNum.length - 1; i > -1; i--) {
+    
+    console.log(arrNum[i]);
+    // console.log("backwards arr2");
+}
 
 
